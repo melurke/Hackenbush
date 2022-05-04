51ea4e7796aa9d7472ee2552324543f26f4bc08a
melurke <101022215+melurke@users.noreply.github.com>
2022-05-04 13:53:16 +0200

    The game of Hackenbush with only green edges

---
+++ game.py
@@ -0,0 +1,43 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import functions
+
+G = nx.Graph()
+G.add_node(0)
+
+end_game = False
+player = 1
+num_of_edges = int(input("How many edges should there be? "))
+edges = []
+point1 = 0
+point2 = 0
+
+for i in range(0, num_of_edges):
+    point1 = int(input(f"What should the first point of the {i+1}. edge be? "))
+    point2 = int(input(f"What should the second point of the {i+1}. edge be? "))
+    if not point1 in G.nodes():
+        G.add_node(point1)
+    if not point2 in G.nodes():
+        G.add_node(point2)
+
+    edges.append((point1, point2))
+
+G.add_edges_from(edges)
+
+while not end_game:
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.show()
+    
+    edge = [int(input("What should the first point of the edge be? ")), int(input("What should the second point of the edge be? "))]
+    try:
+        functions.turn(G, edge)
+        player += 1
+    except:
+        end_game = True
+
+print("")
+
+if player % 2 == 0:
+    print("Player 1 won!")
+else:
+    print("Player 2 won!")
