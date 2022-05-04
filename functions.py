cc75fb4833d602e0ee16628dc28b4ab0bf2e0c86
melurke <101022215+melurke@users.noreply.github.com>
2022-05-04 16:14:45 +0200

    Functions needed for the other programs

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import ast
 import networkx as nx
 
 pos = {
@@ -8,6 +9,15 @@ def mex(K):
     for i in range(0, max(K)+2):
         if not i in K:
             return i
+
+def remove_letters(word):
+    result = []
+    for letter in word:
+        try:
+            result.append(int(letter))
+        except:
+            pass
+    return result
 
 def fall(edges, G):
     falling = []
@@ -29,21 +39,31 @@ def turn(G, edge):
     G.remove_nodes_from(fall_nodes(G.nodes(), G))
     return G
 
-def test_turn(G):
+def test_turn(F):
     next = []
-    F = G.edges()
-    for edge in F:
+    edges = ""
+    nodes = ""
+    G = F.copy()
+    for edge in G.edges():
+        G = F.copy()
         G.remove_edge(edge[0], edge[1])
         G.remove_edges_from(fall(G.edges(), G))
         G.remove_nodes_from(fall_nodes(G.nodes(), G))
-        next.append(G)
+        edges = remove_letters(str(G.edges()))
+        nodes = remove_letters(str(G.nodes()))
+        next.append([nodes, edges])
     return next
 
 def g(G):
     next = test_turn(G)
+
     for follower in next:
-        if not str(follower.edges) in pos:
-            pos[follower.edges] = g(follower)
+        G = nx.Graph()
+        G.add_nodes_from(follower[0])
+        G.add_edges_from(follower[1])
+        if not str(G.edges()) in pos:
+            pos[G.edges()] = g(G)
+        print(G.edges())
 
     for i in range(0, len(next)):
         next[i] = [next[i], g(next[i])]
