88950fe8e21b4d1ac3f33c69746a4e67bf34ed47
melurke <101022215+melurke@users.noreply.github.com>
2022-05-05 13:12:55 +0200

    The game of Hackenbush

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import functions
 
-G = nx.Graph()
+G = nx.MultiGraph()
 G.add_node(0)
 
 end_game = False
@@ -11,6 +11,9 @@ num_of_edges = int(input("How many edges should there be? "))
 edges = []
 point1 = 0
 point2 = 0
+options = {
+    'edge_color': 'green'
+}
 
 for i in range(0, num_of_edges):
     point1 = int(input(f"What should the first point of the {i+1}. edge be? "))
@@ -22,25 +25,25 @@ for i in range(0, num_of_edges):
 
     edges.append((point1, point2))
 
-G.add_edges_from(edges)
+G.add_edges_from(edges, color='green')
 
 G.remove_edges_from(functions.fall(G.edges(), G))
 G.remove_nodes_from(functions.fall_nodes(G.nodes(), G))
 
 while not end_game:
-    nx.draw(G, with_labels=True, font_weight='bold')
+    nx.draw(G, with_labels=True, font_weight='bold', **options)
     plt.show()
     
-    edge = [int(input("What should the first point of the edge be? ")), int(input("What should the second point of the edge be? "))]
+    edge = (int(input("What should the first point of the edge be? ")), int(input("What should the second point of the edge be? ")))
+    new_edges = functions.turn(G.edges(), edge)
     try:
-        new_edges = functions.turn(G.edges(), edge)
         G.clear()
         G.add_node(0)
         G.add_edges_from(new_edges)
         player += 1
     except:
         end_game = True
-
+        
 print("")
 
 if player % 2 == 0:
