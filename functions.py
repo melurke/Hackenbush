dd9f6cc9783571b3e850335c9e95cff9dcd69e51
melurke <101022215+melurke@users.noreply.github.com>
2022-05-05 12:28:38 +0200

    Functions for the other programs

--- functions.py
+++ functions.py
@@ -10,15 +10,6 @@ def mex(K):
         if not i in K:
             return i
 
-def remove_letters(word):
-    result = []
-    for letter in word:
-        try:
-            result.append(int(letter))
-        except:
-            pass
-    return result
-
 def fall(edges, G):
     falling = []
     for edge in edges:
@@ -29,43 +20,25 @@ def fall(edges, G):
 def fall_nodes(nodes, G):
     falling = []
     for node in nodes:
-        if not nx.has_path(G, 0, node):
+        if not nx.has_path(G, node, 0):
             falling.append(node)
     return falling
 
-def turn(G, edge):
+def turn(edges, edge):
+    G = nx.Graph()
+    G.add_edges_from(edges)
     G.remove_edge(edge[0], edge[1])
     G.remove_edges_from(fall(G.edges(), G))
     G.remove_nodes_from(fall_nodes(G.nodes(), G))
-    return G
+    return list(G.edges())
 
-def test_turn(F):
+def followers(G):
     next = []
-    edges = ""
-    nodes = ""
-    G = F.copy()
+    test = list(G.edges())
     for edge in G.edges():
-        G = F.copy()
-        G.remove_edge(edge[0], edge[1])
-        G.remove_edges_from(fall(G.edges(), G))
-        G.remove_nodes_from(fall_nodes(G.nodes(), G))
-        edges = remove_letters(str(G.edges()))
-        nodes = remove_letters(str(G.nodes()))
-        next.append([nodes, edges])
-    return next
+        edges = test.copy()
+        next.append(turn(edges, edge))
 
-def g(G):
-    next = test_turn(G)
-
-    for follower in next:
-        G = nx.Graph()
-        G.add_nodes_from(follower[0])
-        G.add_edges_from(follower[1])
-        if not str(G.edges()) in pos:
-            pos[G.edges()] = g(G)
-        print(G.edges())
-
-    for i in range(0, len(next)):
-        next[i] = [next[i], g(next[i])]
-    
-    return mex(next[:][1])
+G = nx.Graph()
+G.add_edges_from([(0, 1), (0, 2), (1, 2)])
+print(followers(G))
