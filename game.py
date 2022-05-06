b2807eeb813578dad2efd7bd1e04a4230570c965
melurke <101022215+melurke@users.noreply.github.com>
2022-05-06 08:47:22 +0200

    The game of Hackenbush

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import functions
 
 G = nx.MultiGraph()
+H = nx.MultiDiGraph()
 G.add_node(0)
 
 end_game = False
@@ -25,13 +26,23 @@ for i in range(0, num_of_edges):
 
     edges.append((point1, point2))
 
-G.add_edges_from(edges, color='green')
+G.add_edges_from(edges)
 
 G.remove_edges_from(functions.fall(G.edges(), G))
 G.remove_nodes_from(functions.fall_nodes(G.nodes(), G))
 
 while not end_game:
-    nx.draw(G, with_labels=True, font_weight='bold', **options)
+    H = G.copy()
+    test = list(G.edges()).copy()
+    H.remove_edges_from(G.edges())
+    for edge in functions.double_edges(test):
+        print(edge)
+        H.add_edge(edge[0], edge[1])
+        print(H.edges())
+    pos = nx.spring_layout(H)
+    nx.draw_networkx_nodes(H, pos, node_size = 500)
+    nx.draw_networkx_labels(H, pos)
+    nx.draw_networkx_edges(H, pos, connectionstyle='arc3, rad = 0.1', width = 2, arrows=True)
     plt.show()
     
     edge = (int(input("What should the first point of the edge be? ")), int(input("What should the second point of the edge be? ")))
@@ -42,6 +53,7 @@ while not end_game:
         G.add_node(0)
         G.add_edges_from(new_edges)
         player += 1
+        edges = new_edges.copy()
     except:
         end_game = True
 
