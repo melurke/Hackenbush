995374cb4d43c72ad2e1a7e34f6d52c01ab16202
melurke <101022215+melurke@users.noreply.github.com>
2022-05-06 12:14:14 +0200

    The game of Hackenbush

--- game.py
+++ game.py
@@ -26,23 +26,38 @@ for i in range(0, num_of_edges):
 
     edges.append((point1, point2))
 
-G.add_edges_from(edges)
+for edge in edges:
+    edge1 = edge[0]
+    edge2 = edge[1]
+    G.add_edge(edge1, edge2)
 
 G.remove_edges_from(functions.fall(G.edges(), G))
 G.remove_nodes_from(functions.fall_nodes(G.nodes(), G))
 
 while not end_game:
     H = G.copy()
-    test = list(G.edges()).copy()
     H.remove_edges_from(G.edges())
-    for edge in functions.double_edges(test):
-        print(edge)
+    cool = functions.double_edges(list(G.edges()))
+    for edge in cool:
         H.add_edge(edge[0], edge[1])
-        print(H.edges())
-    pos = nx.spring_layout(H)
-    nx.draw_networkx_nodes(H, pos, node_size = 500)
-    nx.draw_networkx_labels(H, pos)
-    nx.draw_networkx_edges(H, pos, connectionstyle='arc3, rad = 0.1', width = 2, arrows=True)
+    print(cool)
+    print(H.edges())
+
+    pos = nx.random_layout(G)
+    nx.draw_networkx_nodes(G, pos, node_color='b', node_size=100, alpha = 1)
+    ax = plt.gca()
+    for e in G.edges:
+        ax.annotate("",
+                    xy=pos[e[0]], xycoords='data',
+                    xytext=pos[e[1]], textcoords='data',
+                    arrowprops=dict(arrowstyle="-", color="0.5",
+                                    shrinkA=5, shrinkB=5,
+                                    patchA=None, patchB=None,
+                                    connectionstyle="arc3,rad=rrr".replace('rrr',str(0.3*e[2])
+                                    ),
+                                    ),
+                    )
+    plt.axis('off')
     plt.show()
     
     edge = (int(input("What should the first point of the edge be? ")), int(input("What should the second point of the edge be? ")))
@@ -51,7 +66,10 @@ while not end_game:
         new_edges = functions.turn(G.edges(), edge)
         G.clear()
         G.add_node(0)
-        G.add_edges_from(new_edges)
+        for edge in new_edges:
+            edge1 = edge[0]
+            edge2 = edge[1]
+            G.add_edge(edge1, edge2)
         player += 1
         edges = new_edges.copy()
     except:
